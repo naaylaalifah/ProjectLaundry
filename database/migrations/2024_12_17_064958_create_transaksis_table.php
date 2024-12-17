742713733d084f4f76eec6f4df894bb91383f04b
Nayla <naaylaalifah@gmail.com>
2024-12-17 13:57:39 +0700

    add controller model and table

---
+++ database/migrations/2024_12_17_064958_create_transaksis_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transaksis', function (Blueprint $table) {
+            $table->id();
+            $table->integer('pelanggan_id');
+            $table->integer('barang_id');
+            $table->integer('biaya');
+            $table->string('pengantaran');
+            $table->string('status_transaksi');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transaksis');
+    }
+};
